1c1ef063df39b41d464cb784506d7f311f4954af
RyanNavillus <ryanpnavillus@gmail.com>
2020-04-01 01:07:56 -0400

    Add adaboost file

--- adaboost.py
+++ adaboost.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 25 14:57:57 2020
-
-@author: anirban
-"""
 
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import preprocessing
-from sklearn import svm
+from sklearn.ensemble import AdaBoostRegressor
 
 #Preprocessing function 
 def preprocessData(data):
@@ -21,13 +16,11 @@ def preprocessData(data):
     return data
 
 
-data =pd.read_csv('/home/anirban/cs578/Final_project/loan-default-prediction/train_v2.csv')
+data =pd.read_csv('train_v2.csv')
 
-#Create 10k data for just doing whatever. Not sure if this is a good ML approach 
-#Correct me wherever you feel like . Here I am just trying to see how good accuraccy we get for just classification
-#I set loss >0 to 1 and let 0 remain as they are
-a=np.arange(105470)
-np.random.shuffle(a)
+print(len(data))
+a=np.arange(len(data))
+#np.random.shuffle(a)
 
 reduced_train_data = data.iloc[a[0:10000]]
 reduced_test_data = data.iloc[a[10000:20000]]
@@ -36,16 +29,17 @@ reduced_test_data = data.iloc[a[10000:20000]]
 reduced_train_data = preprocessData(reduced_train_data)
 X= reduced_train_data.drop(columns=['loss', 'id'])     #Since loss and id arent part of features
 y= reduced_train_data['loss']
-svm_linear = Pipeline([
-        ("scaler", MinMaxScaler()),
-        ("linear_svc", svm.LinearSVC(C=10, loss='squared_hinge', tol=1e-3, max_iter= 2000, dual=False))])
-svm_linear.fit(X,y)
+#svm_linear = Pipeline([
+#        ("scaler", MinMaxScaler()),
+#        ("linear_svc", svm.LinearSVC(C=10, loss='squared_hinge', tol=1e-3, max_iter= 2000, dual=False))])
+adaboost = AdaBoostRegressor(n_estimators=50, learning_rate=1)
+adaboost.fit(X,y)
 
 
 reduced_test_data = preprocessData(reduced_test_data)
 X_test= reduced_test_data.drop(columns=['loss', 'id'])
 y_test= reduced_test_data['loss']
-y_pred= svm_linear.predict(X_test)
+y_pred= adaboost.predict(X_test)
 
 print (sum(abs(y_test-y_pred)))
 print (len(np.where(y_test == 1)[0]))    #Place where loss is > 0  
