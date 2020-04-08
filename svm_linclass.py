8a8948d99a227c86701b14ac449684780cf6a53c
Anirban Dutta <44774039+anirban1195@users.noreply.github.com>
2020-04-08 19:57:41 -0400

    Update svm_linclass.py
    Slight changes to run k-fold and get both mean and variance

--- svm_linclass.py
+++ svm_linclass.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 25 14:57:57 2020
+Created on Wed Apr  8 09:32:48 2020
 
+@author: anirban
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 14:57:57 2020
 @author: anirban
 """
 
@@ -12,6 +19,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import preprocessing
 from sklearn import svm
+from kfold import *
+from score import *
 
 #Preprocessing function 
 def preprocessData(data):
@@ -21,34 +30,34 @@ def preprocessData(data):
     return data
 
 
-data =pd.read_csv('train_v2.csv')
-
-#Create 10k data for just doing whatever. Not sure if this is a good ML approach 
-#Correct me wherever you feel like . Here I am just trying to see how good accuraccy we get for just classification
-#I set loss >0 to 1 and let 0 remain as they are
-a=np.arange(105470)
-np.random.shuffle(a)
-
-#reduced_train_data = data.iloc[a[0:10000]]
-#reduced_test_data = data.iloc[a[10000:20000]]
-
-[reduced_train_data, reduced_val_data, reduced_test_data] = getdata(0, 10, data)
-
-#Training with linear SVM
-reduced_train_data = preprocessData(reduced_train_data)
-X= reduced_train_data.drop(columns=['loss', 'id'])     #Since loss and id arent part of features
-y= reduced_train_data['loss']
-svm_linear = Pipeline([
-        ("scaler", MinMaxScaler()),
-        ("linear_svc", svm.LinearSVC(C=10, loss='squared_hinge', tol=1e-3, max_iter= 2000, dual=False))])
-svm_linear.fit(X,y)
-
-
-reduced_val_data = preprocessData(reduced_val_data)
-X_test= reduced_val_data.drop(columns=['loss', 'id'])
-y_test= reduced_val_data['loss']
-y_pred= svm_linear.predict(X_test)
-
-print (sum(abs(y_test-y_pred)))
-print (len(np.where(y_test == 1)[0]))    #Place where loss is > 0  
+data =pd.read_csv('/home/anirban/cs578/Final_project/loan-default-prediction/train_v2.csv')
+k=10
+accuracyArr =[]
+# pca_features = 40 #Use only when using PCA
+for k_current in range(k):
+    
+    [reduced_train_data, reduced_val_data, reduced_test_data] = getdata(k_current, k, data)
+    
+    #Training with linear SVM
+    reduced_train_data = preprocessData(reduced_train_data)
+    X= reduced_train_data.drop(columns=['loss', 'id'])     #Since loss and id arent part of features
+    y= reduced_train_data['loss']
+    svm_linear = Pipeline([
+            ("scaler", MinMaxScaler()),
+            #("pca", PCA(n_components=pca_features)),
+            ("linear_svc", svm.LinearSVC(C=1, loss='squared_hinge', tol=1e-3, max_iter= 10, dual=False))])
+    svm_linear.fit(X,y)
+    
+    
+    reduced_val_data = preprocessData(reduced_val_data)
+    X_test= reduced_val_data.drop(columns=['loss', 'id'])
+    y_test= reduced_val_data['loss']
+    y_pred= svm_linear.predict(X_test)
+    
+    accuracy = getScore(y_pred, y_test)
+    accuracyArr.append(accuracy)
+    
+mean, variance = getkFoldScore(accuracyArr)
+print (mean, variance)
+#874.4 2117.2400000000007    
 
