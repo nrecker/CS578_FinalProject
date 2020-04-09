837918b3e7502b532bd980f8e5431debc40d707a
Anirban Dutta <44774039+anirban1195@users.noreply.github.com>
2020-04-08 20:01:15 -0400

    Gets accuracy and mean and variance of k fold

---
+++ score.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 20:14:57 2020
+
+@author: anirban
+"""
+
+import numpy as np
+import pandas as pd
+
+#Returns the mae score
+def getScore(y, y_pred):
+    #Convert to numpy array for faster operation
+    y = np.array(y)
+    y_pred = np.array(y_pred)
+    if(len(y) != len(y_pred)):
+        print ('Length mismatch error!')
+        return -1
+    #This competition is evaluated on the mean absolute error (MAE)
+    score = len(np.where (y != y_pred)[0])
+    return score
+
+#Takes an array of mae scores for all ks for a given algorithm 
+# and returns the mean and vairance
+def getkFoldScore(scoreArr):
+    scoreArr =np.array(scoreArr)
+    mean = np.mean(scoreArr)
+    
+    variance = np.std(scoreArr) ** 2
+    
+    return mean, variance
+    
+    
