b2c8315f619ecba80d87720ba241170427866c13
Anirban Dutta <44774039+anirban1195@users.noreply.github.com>
2020-04-19 11:12:51 -0400

    Update kfold.py

--- kfold.py
+++ kfold.py
@@ -5,22 +5,33 @@ from sklearn.preprocessing import StandardScaler
 
 # Partitions full dataset into training, validation, and test data
 # 0 <= k < maxk
-def getdata(k, maxk, data):
-    n = len(data)
-    a=np.arange(n)
-    nTrainVal = n - n//10
-    nVal = nTrainVal//maxk  # k=maxk-1 is actually a little larger because of rounding
-
-    if k == maxk-1: # handled separately because last k gets the roundoff
-        train = data.iloc[a[:k*nVal]]
-        val = data.iloc[a[k*nVal:nTrainVal]]
-    else:
-        train = data.iloc[np.concatenate((a[:k*nVal], a[(k+1)*nVal:nTrainVal]))]
-        val = data.iloc[a[k*nVal:(k+1)*nVal]]
+def getdata(k, maxk, data, kFold_flag = 1):
+    if(kFold_flag == 1):
+        n = len(data)
+        a=np.arange(n)
+        nTrainVal = n - n//10
+        nVal = nTrainVal//maxk  # k=maxk-1 is actually a little larger because of rounding
     
-    test = data.iloc[a[nTrainVal:]]
+        if k == maxk-1: # handled separately because last k gets the roundoff
+            train = data.iloc[a[:k*nVal]]
+            val = data.iloc[a[k*nVal:nTrainVal]]
+        else:
+            train = data.iloc[np.concatenate((a[:k*nVal], a[(k+1)*nVal:nTrainVal]))]
+            val = data.iloc[a[k*nVal:(k+1)*nVal]]
         
-    return train, val, test
+        test = data.iloc[a[nTrainVal:]]
+            
+        return train, val, test
+    else:
+        #When we want all the  training and test data and NO validation 
+        n = len(data)
+        a=np.arange(n)
+        nTrainVal = n - n//10
+        nVal = nTrainVal//maxk  # k=maxk-1 is actually a little larger because of rounding
+        train = data.iloc[a[0:nTrainVal]]
+        test = data.iloc[a[nTrainVal:]]
+        
+        return train,test
 
 #testing code
-[a,b,c] = getdata(2,10,pd.read_csv('train_v2.csv'))
+#[a,b,c] = getdata(2,10,pd.read_csv('train_v2.csv'))
